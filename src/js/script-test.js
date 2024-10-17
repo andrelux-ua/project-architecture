4a16c9e1b2fded5aa161e6dcd7f15a45b347c12e
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2024-10-17 12:53:02 +0300

    sections

---
+++ src/js/script-test.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const greeting = document.createElement("h1");
+    greeting.textContent = "Ласкаво просимо на наш сайт!";
+    document.body.appendChild(greeting);
+});
